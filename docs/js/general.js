c11cd7f1f1f7e04dd683243d8c6442c49454c8ff
TravisA9 <travisashworth2007@gmail.com>
2020-12-29 23:56:03 +0630

    test

---
+++ docs/js/general.js
@@ -0,0 +1,18 @@
+function openpage(evt, pageName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("page");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+    }
+    document.getElementById(pageName).style.display = "block";
+    evt.currentTarget.className += " w3-red";
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
